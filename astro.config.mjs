1a21030f2da42e3701843b5effa25e32f6610215
Alvaro <agilsansinena@gmail.com>
2025-12-30 15:58:13 +0100

    feat: add base layout with SEO, analytics, and sitemap integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.mielhurdegatina.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
